4c7802ab126d1b887fdaf89e929e7d8971a12447
YevaVlasova <eva-krechetova@ukr.net>
2023-04-18 18:29:34 +0300

    beginning

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import LogoIcon from '../icons/LogoIcon';
+import './Header.scss';
+
+function Header() {
+  return (
+    <div className='logo'>
+        <LogoIcon />
+        <div className="logo__text">PNFT Market</div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import './Login.scss';
+
+function Login() {
+  return (
+    <div className='login'>
+        <div className="login__title">NFT Access</div>
+        <div className="login__desc">Please fill your detail to access your account.</div>
+    </div>
+  );
+}
+
+export default Login;
